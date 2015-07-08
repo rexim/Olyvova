05f6b704a934b3b2c062b5c88cb54ba5a81dbdcb
rexim <reximkut@gmail.com>
2015-07-09 00:46:20 +0600

    Add skeleton for new site generator script

---
+++ Builder.pm
@@ -0,0 +1,27 @@
+package Olyvova::Builder;
+
+use 5.10.1;
+
+use strict;
+use warnings;
+use utf8;
+
+use Exporter qw(import);
+
+our @EXPORT_OK = qw(single multiple pagination build);
+
+sub single($$$) {
+    
+}
+
+sub multiple($$) {
+    
+}
+
+sub pagination($$$) {
+    
+}
+
+sub build($) {
+    
+}
